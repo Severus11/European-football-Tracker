7106fb308c257894da630335d112033567ba3c3d
Severus11 <parthsgupta@gmail.com>
2021-03-16 13:19:59 +0530

    added support for PL

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np 
 import pandas as pd 
 import base64
-import googlesearch
+from googlesearch import search
 import matplotlib.pyplot as plt 
 
 st.title('Premier League Tracker')
@@ -14,18 +14,39 @@ st.markdown('''This app performs simple webscraping of Premier League teams data
 st.sidebar.header('User input features')
 
 list_league = ['Premier League', 'Bundesliga', 'La Liga','Ligue 1', 'Serie A']
-selected_league = st.sidebar.selectbox('Select league', list_league,list_league )
-selected_year = st.sidebar.selectbox('Choose Season', list(reversed(range(1950, 2020))))
-
-@st.cache
-def load_data(league, year):
-    query= league + 'reference' + year
+selected_league = st.sidebar.selectbox('Select league', ('Premier League', 'Bundesliga', 'La Liga','Ligue 1', 'Serie A'))
+selected_year = st.sidebar.selectbox('Choose Season', list(reversed(range(1950, 2021))))
+def load_query(query):
     for j in search(query, tld="com", num=1, stop=1, pause =2):
         url = j
-    html = pd.read_html(url, header =1)
+    return url
+@st.cache
+def load_data(league, year):
+    query= league + 'reference' + str(year)    
+    url = load_query(query)
+    html = pd.read_html(url, header =0)
     df= html[0]
-    raw = df.drop(df[df.Age == 'Age'].index) 
-    raw = raw.fillna(0)
-    playerstats = raw.drop(['Rk'], axis=1)
-    return playerstats
-playerstats = load_data(selected_year)
+    raw=df.drop(['Attendance','Last 5', 'Goalkeeper', 'Notes','GD','xGA','xGD'], axis = 1)
+    rankings_table=raw.rename(columns={'Squad':'Teams','Rk':'Pos'})
+    return rankings_table
+
+league_rankings= load_data(selected_league,selected_year)
+
+teams = sorted(league_rankings.Teams.unique())
+selected_team = st.sidebar.multiselect('Teams', teams, teams)
+
+df_selected_team = league_rankings[(league_rankings.Teams.isin(selected_team))]
+
+st.header('Display Player Stats of Selected Team(s)')
+st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
+st.dataframe(df_selected_team)
+
+# Download NBA player stats data
+# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
+def filedownload(df):
+    csv = df.to_csv(index=False)
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    href = f'<a href="data:file/csv;base64,{b64}" download="standings.csv">Download CSV File</a>'
+    return href
+
+st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
--- test.py
+++ test.py
@@ -10,11 +10,11 @@ for j in search(query, tld="com", num=1, stop=1, pause =2):
     url = j
 html = pd.read_html(url, header =0)
 df= html[0]
-raw=df.drop(['Attendance','Last 5', 'Goalkeeper', 'Notes','GD','xGA','xGD'] axis = 1)
-
+raw=df.drop(['Attendance','Last 5', 'Goalkeeper', 'Notes','GD','xGA','xGD'], axis = 1)
+raw=-raw.rename(columns={'Squad':'Teams','Rk':'Pos'})
 #raw = df.drop(df[df.Age == 'Age'].index) 
 #raw = raw.fillna(0)
 #playerstats = raw.drop(['Rk'], axis=1)
-print()
+print(raw)
 
     
