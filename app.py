69b59be5d253cb0824eadc69e8abfb33ec64dd06
Severus11 <parthsgupta@gmail.com>
2021-03-17 18:34:26 +0530

    added bg img

--- app.py
+++ app.py
@@ -3,24 +3,28 @@ import numpy as np
 import pandas as pd 
 import base64
 from googlesearch import search
-import matplotlib.pyplot as plt 
+from PIL import Image 
 
-st.title('Premier League Tracker')
+image = Image.open('wc1753693 (2).jpg')
 
-st.markdown('''This app performs simple webscraping of Premier League teams data!
+
+
+st.title('Premier football Tracker')
+
+st.markdown('''Now keep a close eye on your favorite teams across Europe's top 5 leagues ! 
 
 * **Data source:** [fbref.com](https://fbref.com/en/comps/9/Premier-League-Stats).''')
+st.image(image, use_column_width=True)
 
 st.sidebar.header('User input features')
 
-list_league = ['Premier League', 'Bundesliga', 'La Liga','Ligue 1', 'Serie A']
 selected_league = st.sidebar.selectbox('Select league', ('Premier League', 'Bundesliga', 'La Liga','Ligue 1', 'Serie A'))
-selected_year = st.sidebar.selectbox('Choose Season', list(reversed(range(1950, 2020 ))))
+selected_year = st.sidebar.selectbox('Choose Season', list(reversed(range(1950, 2021))))
 
 @st.cache
 def load_data(league, year):
     global url
-    query= league + 'reference' + str(year) 
+    query= league + 'reference' + str(year-1) 
     for j in search(query, tld="com", num=1, stop=1, pause =1):
         url = j   
     #url = load_query(query)
@@ -31,22 +35,20 @@ def load_data(league, year):
     return rankings_table
 
 league_rankings= load_data(selected_league,selected_year)
-print(league_rankings)
+#print(league_rankings)
 
 teams = sorted(league_rankings.Teams.unique())
 selected_team = st.sidebar.multiselect('Teams', teams, teams)
 
 df_selected_team = league_rankings[(league_rankings.Teams.isin(selected_team))]
 
-st.header('Display Player Stats of Selected Team(s)')
+st.header('Display Team Stats of the selected Leagues')
 st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
 st.dataframe(df_selected_team)
 
-# Download NBA player stats data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
 def filedownload(df):
     csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
+    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes 
     href = f'<a href="data:file/csv;base64,{b64}" download="standings.csv">Download CSV File</a>'
     return href
 
